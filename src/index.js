ee25a9b948da82def685d0f14e66e90b54f9c1fd
yodit <yoditabebe9391gmail.com>
2023-02-24 10:07:48 +0300

    add remove and add todo-list

--- src/index.js
+++ src/index.js
@@ -8,28 +8,98 @@ const addTodo = document.querySelector('.add-todo');
 const todoList = document.querySelector('.todo-list');
 const completeAll = document.querySelector('.completeAll');
 
-const todos = [{ description: 'Wash clothes', completed: false, index: 1 },
-  { description: 'Make dinner', completed: false, index: 2 },
-  { description: 'wash dishes', completed: false, index: 3 },
-  { description: 'clean house', completed: false, index: 4 }];
 
-todaysTodo.innerHTML = `<h4 class="title">Today's To do</h4>
-<button class="refreshBtn"><img src="${refresh}" alt=""></button>`;
+let todos = [{ description: 'Wash clothes', completed: false, id: '1' },
+  { description: 'Make dinner', completed: false, id: '2' },
+  { description: 'wash dishes', completed: false, id: '3' },
+  { description: 'clean house', completed: false, id: '4' }];
 
-addTodo.innerHTML = `<input type="text" class="input-todo" placeholder="Add to your list...">
-<button class="addBtn"><img src="${add}" alt=""></button>`;
+render();
 
-todos.forEach((todo) => {
-  const list = document.createElement('li');
-  list.className = 'list';
-  list.innerHTML = `
-  <input id="${todo.index}" class="check" type="checkbox">
-  <span class="todo-title">${todo.description}</span>
-  <button id="${todo.index}" class="more-btn"><img src="${morevert}" alt="more-vertical-icon"></button>
-  `;
-  todoList.appendChild(list);
+class CreateTodo {
+  constructor(description, id) {
+    this.description = description;
+    this.id = id;
+    this.completed = false;
+  }
+}
+
+function render() {
+  todoList.innerHTML = '';
+  todaysTodo.innerHTML = `<h4 class="title">Today's To do</h4>
+  <button class="refreshBtn"><img src="${refresh}" alt=""></button>`;
+  addTodo.innerHTML = `<input type="text" class="input-todo" placeholder="Add to your list...">
+  <button class="addBtn"><img src="${add}" alt=""></button>`;
+  todos.forEach((todo) => {
+    const list = document.createElement('li');
+    list.className = 'list';
+    list.innerHTML = `
+    <input id="${todo.id}" class="check" type="checkbox">
+    <span class="todo-title">${todo.description}</span>
+    <button class="more-btn"><img id="${todo.id}" src="${morevert}" alt="more-vertical-icon"></button>
+    `;
+    todoList.appendChild(list);
+  });
+  completeAll.innerHTML = `<input class="check" type="checkbox">
+  <span class="todo-title bold">Complete To Do list project</span>
+  <button class="more-btn"><img src="${morevert}" alt="more-vertical-icon"></button>`;
+
+  const moreBtn = document.querySelectorAll('.more-btn > img');
+  moreBtn.forEach(function(more) {
+    more.addEventListener('click', function(e) {
+      moreVert();
+     
+      // const removed = e.target;
+      // const id = removed.id;
+      // removeTask(id);
+      // render();
+    })
+  });
+}
+
+const lists = document.querySelectorAll('.list');
+
+function moreVert() {
+  console.log('hello');
+  const elem = document.createElement('div');
+  elem.className = 'click';
+  elem.innerHTML = 
+    `<div class="click-more">
+      <button class="delete fa-sharp fa-solid fa-trash"></button>
+      <button class="edit fa-regular fa-pen-to-square"></button>
+    </div>`
+  lists.appendChild(elem);
+}
+
+moreVert();
+
+  function addTask() {
+    const newDescription = inputTodo.value;
+    const id = `${new Date().getTime()}`;
+  const newTodo = new CreateTodo(newDescription, id);
+  todos.push(newTodo);
+}
+
+function removeTask(idToDelete) {
+  todos = todos.filter(function(todo) {
+    if(todo.id === idToDelete) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  })
+}
+
+
+const inputTodo = document.querySelector('.input-todo');
+const addBtn = document.querySelector('.addBtn');
+
+
+addBtn.addEventListener('click', function(e){
+  addTask();
+  render();
 });
 
-completeAll.innerHTML = `<input class="check" type="checkbox">
-<span class="todo-title bold">Complete To Do list project</span>
-<button class="more-btn"><img src="${morevert}" alt="more-vertical-icon"></button>`;
+
+
